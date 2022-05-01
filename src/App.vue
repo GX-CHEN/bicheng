369e6b6c654955bedbed36843e1d6cd3d1748bf0
Gongxia Chen <chengongxia1990@gmail.com>
2022-05-02 02:47:26 +0800

    完成UI建设

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-scroll-snap :fullscreen="true">
+      <div class="item" id="one" style="background: #fffef8">
+        <div class="gradient-text" style="margin-top: 20vh">
+          生日快乐，我的宝贝
+        </div>
+        <BlinkRose />
+        <div class="gradient-text subtitle">一朵小花送给可爱的你</div>
+        <div class="instruction">
+          请向下滑
+          <div>&#8595;</div>
+        </div>
+      </div>
+      <div class="item" id="two">
+        <div class="gradient-text" style="font-size: 28px; margin-top: 8vh">
+          这是我怦怦跳的心
+        </div>
+        <LoveHeart />
+        <div
+          class="gradient-text subtitle"
+          style="margin-top: 20px; margin-bottom: 20px"
+        >
+          心为何怦怦跳？因为这位姑娘
+        </div>
+        <img src="./assets/profile.jpg" alt="profile" class="profile-avatar" />
+        <div class="instruction">
+          请向下滑
+          <div>&#8595;</div>
+        </div>
+      </div>
+      <div class="item" id="three">
+        <YouAre />
+      </div>
+    </vue-scroll-snap>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueScrollSnap from "vue-scroll-snap";
+import LoveHeart from "@/components/LoveHeart";
+import BlinkRose from "@/components/BlinkRose";
+import YouAre from "@/components/YouAre";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { VueScrollSnap, LoveHeart, BlinkRose, YouAre },
+};
 </script>
 
 <style>
@@ -24,5 +56,54 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.item {
+  position: relative;
+}
+
+/* Gradient Text*/
+.gradient-text {
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 36px;
+  font-weight: 400;
+  letter-spacing: 2px;
+  color: #f35626;
+  background-image: linear-gradient(92deg, #f35626, #feab3a);
+  -webkit-background-clip: text !important;
+  -webkit-text-fill-color: transparent !important;
+  animation: hue 10s infinite linear !important;
+  margin-bottom: 6px;
+}
+
+.subtitle {
+  font-size: 20px;
+  -webkit-background-clip: text !important;
+  -webkit-text-fill-color: transparent !important;
+  color: #2c3e50;
+  animation: hue 3s infinite linear !important;
+  background-image: linear-gradient(50deg, #134857, #3b818c);
+}
+
+.instruction {
+  width: 100vw;
+  text-align: center;
+  position: absolute;
+  bottom: 36px;
+}
+
+@keyframes hue {
+  from {
+    filter: hue-rotate(0deg);
+  }
+  to {
+    filter: hue-rotate(-360deg);
+  }
+}
+
+.profile-avatar {
+  width: 180px;
+  height: 180px;
+  border-radius: 90px;
 }
 </style>
---
+++ src/components/LoveHeart.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <div class="heart"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoveHeart.vue",
+};
+</script>
+<style scoped>
+.container {
+  margin-top: 30px;
+}
+.heart {
+  position: relative;
+  width: 160px;
+  height: 146px;
+  animation: heartbeat 1s infinite;
+  margin: 0 auto;
+}
+.heart:before,
+.heart:after {
+  position: absolute;
+  content: "";
+  left: 80px;
+  top: 0;
+  width: 80px;
+  height: 128px;
+  background: #ec2b24;
+  -moz-border-radius: 80px 80px 0 0;
+  border-radius: 80px 80px 0 0;
+  -webkit-transform: rotate(-45deg);
+  -moz-transform: rotate(-45deg);
+  -ms-transform: rotate(-45deg);
+  -o-transform: rotate(-45deg);
+  transform: rotate(-45deg);
+  -webkit-transform-origin: 0 100%;
+  -moz-transform-origin: 0 100%;
+  -ms-transform-origin: 0 100%;
+  -o-transform-origin: 0 100%;
+  transform-origin: 0 100%;
+}
+.heart:after {
+  left: 0;
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  transform: rotate(45deg);
+  -webkit-transform-origin: 100% 100%;
+  -moz-transform-origin: 100% 100%;
+  -ms-transform-origin: 100% 100%;
+  -o-transform-origin: 100% 100%;
+  transform-origin: 100% 100%;
+}
+
+@keyframes heartbeat {
+  0% {
+    transform: scale(0.75);
+  }
+  20% {
+    transform: scale(1);
+  }
+  40% {
+    transform: scale(0.75);
+  }
+  60% {
+    transform: scale(1);
+  }
+  80% {
+    transform: scale(0.75);
+  }
+  100% {
+    transform: scale(0.75);
+  }
+}
+</style>
